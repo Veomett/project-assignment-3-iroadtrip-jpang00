9ca89b85797c1a70f3686d07a0a38575dbacc830
jpang00 <jcpang@dons.usfca.edu>
2023-12-01 16:01:46 -0800

    read state names

--- IRoadTrip.java
+++ IRoadTrip.java
@@ -1,11 +1,51 @@
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class IRoadTrip {
+    private Map<String, Map<String, Integer>> bordersMap;
+    private Map<String, Map<String, Integer>> distancesMap;
+    private Map<String, String> countryToStateIdMap;
 
+    public IRoadTrip (String [] args) throws IOException {
 
-    public IRoadTrip (String [] args) {
-        // Replace with your code
+        String bordersFile = args[0];
+        String distancesFile = args[1];
+        String stateNamesFile = args[2];
+
+        bordersMap = new HashMap<>();
+        distancesMap = new HashMap<>();
+        countryToStateIdMap = new HashMap<>();
+
+        //reads borders.txt
+        readBordersFile(bordersFile);
+        //reads capdist.csv
+        readDistancesFile(distancesFile);
+        //reads state_name.tsv
+        readStateNamesFile(stateNamesFile);
     }
+
+
+
+    private void readStateNamesFile(String fileName) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            // Process each line from state_name.tsv
+            String[] parts = line.split("\t");
+            if (parts.length == 5 && "2020-12-31".equals(parts[4])) {
+                String stateId = parts[1];
+                String stateNumber = parts[0];
+                String countryName = parts[2];
+                countryToStateIdMap.put(stateId, countryName);
+            }
+        }
+        reader.close();
+    }
+
 
 
     public int getDistance (String country1, String country2) {
@@ -16,14 +56,19 @@ public class IRoadTrip {
 
     public List<String> findPath (String country1, String country2) {
         // Replace with your code
+
+
         return null;
     }
 
 
     public void acceptUserInput() {
         // Replace with your code
+
         System.out.println("IRoadTrip - skeleton");
     }
+
+
 
 
     public static void main(String[] args) {
